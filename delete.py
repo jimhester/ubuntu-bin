9ff83dd5e596fe632b186c889b6082a4a9a22535
Jim Hester <james.f.hester@gmail.com>
2016-06-07 15:47:02 -0400

    [build texlive]

--- delete.py
+++ delete.py
@@ -2,4 +2,4 @@ import json, sys, os
 d = json.load(sys.stdin)['assets']
 for a in d:
     if a['name'].startswith(sys.argv[1]):
-        os.system('curl -u yihui:' + os.getenv('GH_TOKEN') + ' -X DELETE ' + a['url'])
+        os.system('curl -u jimhester:' + os.getenv('GH_TOKEN') + ' -X DELETE ' + a['url'])
